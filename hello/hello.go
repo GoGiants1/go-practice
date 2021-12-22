29248e11829a948ef5b4e9387af8beea38759e11
GoGiants1 <chooi221@snu.ac.kr>
2021-12-22 17:05:42 +0900

    Done custom module tutorial

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"example.com/greetings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Get a greeting message and print it.
+	message := greetings.Hello("GoGiants1")
+	fmt.Println(message)
 }
