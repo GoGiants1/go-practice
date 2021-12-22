9583c0880501d8554194e1ceb9dae3649181aac7
GoGiants1 <chooi221@snu.ac.kr>
2021-12-22 18:35:31 +0900

    Done err handling

--- greetings/grettings.go
+++ greetings/grettings.go
@@ -1,11 +1,21 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+
+	"fmt"
+)
 
 // Hello returns a greetings for the named person.
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If name was received
 	// Return a greeting that embeds the name in a msg.
 	msg := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return msg
+	return msg, nil
 }
