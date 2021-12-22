597241a48e2d646d50ab9be7e1aaf50e9472676e
GoGiants1 <chooi221@snu.ac.kr>
2021-12-22 22:16:04 +0900

    Done random greeting

--- greetings/grettings.go
+++ greetings/grettings.go
@@ -2,8 +2,9 @@ package greetings
 
 import (
 	"errors"
-
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello returns a greetings for the named person.
@@ -16,6 +17,26 @@ func Hello(name string) (string, error) {
 
 	// If name was received
 	// Return a greeting that embeds the name in a msg.
-	msg := fmt.Sprintf("Hi, %v. Welcome!", name)
+	msg := fmt.Sprintf(randomFormat(), name)
 	return msg, nil
+}
+
+// init sets initial values for variables used in the function.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
